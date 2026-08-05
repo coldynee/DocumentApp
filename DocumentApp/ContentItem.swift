ca5393668c89e49ca8b363c67eefaa52d01a784a
coldynee <coldynee@gmail.com>
2026-08-05 05:56:42 +0300

    Initial commit

---
+++ DocumentApp/ContentItem.swift
@@ -0,0 +1,8 @@
+//
+//  ContentItem.swift
+//  DocumentApp
+//
+//  Created by Никита Морозов on 05.08.2026.
+//
+
+import Foundation
---
+++ DocumentApp/FileManagerService.swift
@@ -0,0 +1,8 @@
+//
+//  FileManagerService.swift
+//  DocumentApp
+//
+//  Created by Никита Морозов on 05.08.2026.
+//
+
+import Foundation
